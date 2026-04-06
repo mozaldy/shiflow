69407db3524ae9d31628a1607e21b53db4d55ee2
mozaldy <moxaldy@gmail.com>
2026-04-06 17:47:16 +0700

    initialize: create an iOS project with Swift and SwiftUI
    for Shiflow, a guitar learning app.

---
+++ shiflow/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  shiflow
+//
+//  Created by Mohammad Rizaldy Ramadhan on 06/04/26.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ContentView: View {
+
+    var body: some View {
+        Text("Welcome to Shiflow!")
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ shiflow/shiflowApp.swift
@@ -0,0 +1,18 @@
+//
+//  shiflowApp.swift
+//  shiflow
+//
+//  Created by Mohammad Rizaldy Ramadhan on 06/04/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct shiflowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
