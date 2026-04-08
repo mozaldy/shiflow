aaf195a6a131d76c022110af51dc3f6e99cb2286
thereuwuw <theressathebez.15@gmail.com>
2026-04-08 18:08:11 +0700

    Make MainStudyPage

---
+++ shiflow/Pages/StudyPage/MainStudyPage/MainStudyPage.swift
@@ -0,0 +1,68 @@
+//
+//  MainStudyPage.swift
+//  shiflow
+//
+//  Created by Theressa Natasha Thebez on 08/04/26.
+//
+
+import Foundation
+import SwiftUI
+
+struct MainStudyPage: View {
+    @State var selectedChord = "Am"
+    let chords = ["Am", "C", "D", "F", "Em", "G"]
+    @Environment(\.dismiss) var dismiss
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: 30) {
+            HStack {
+                Button {
+                    dismiss()
+                } label: {
+                    HStack {
+                        Image(systemName: "arrow.backward")
+                    }
+                    .padding(.horizontal, 16)
+                    .padding(.vertical, 16)
+                    .background(.brown)
+                    .foregroundStyle(.white)
+                    .clipShape(Capsule())
+                }
+
+                Picker("Chord", selection: $selectedChord) {
+                    ForEach(chords, id: \.self) { chord in
+                        Text(chord)
+                    }
+                }
+                .pickerStyle(.segmented)
+                .frame(width: 650)
+            }
+            .padding(.horizontal)
+
+            HStack(spacing: 22) {
+                ChordCard(
+                    imageName: getChordImage(type: "finger"),
+                    title: "Finger Position"
+                )
+
+                ChordCard(
+                    imageName: getChordImage(type: "exercise"),
+                    title: "Finger Push Up Exercise"
+                )
+
+                ChordCard(
+                    imageName: getChordImage(type: "strum"),
+                    title: "How to Strum"
+                )
+            }
+            .frame(maxWidth: .infinity, alignment: .center)
+            .padding(.leading, 50)
+
+        }
+
+    }
+}
+
+#Preview {
+    MainStudyPage()
+}
---
+++ shiflow/Pages/StudyPage/MainStudyPage/getChordImage.swift
@@ -0,0 +1,40 @@
+//
+//  getChordImage.swift
+//  shiflow
+//
+//  Created by Theressa Natasha Thebez on 08/04/26.
+//
+
+extension MainStudyPage {
+    
+    func getChordImage(type: String) -> String {
+        switch (selectedChord, type) {
+        case ("Am", "finger"): return "am_finger"
+        case ("Am", "exercise"): return "am_exercise"
+        case ("Am", "strum"): return "am_strum"
+
+        case ("C", "finger"): return "c_finger"
+        case ("C", "exercise"): return "c_exercise"
+        case ("C", "strum"): return "c_strum"
+
+        case ("D", "finger"): return "d_finger"
+        case ("D", "exercise"): return "d_exercise"
+        case ("D", "strum"): return "d_strum"
+
+        case ("F", "finger"): return "f_finger"
+        case ("F", "exercise"): return "f_exercise"
+        case ("F", "strum"): return "f_strum"
+
+        case ("Em", "finger"): return "em_finger"
+        case ("Em", "exercise"): return "em_exercise"
+        case ("Em", "strum"): return "em_strum"
+
+        case ("G", "finger"): return "g_finger"
+        case ("G", "exercise"): return "g_exercise"
+        case ("G", "strum"): return "g_strum"
+            
+        default:
+            return "chord"
+        }
+    }
+}
