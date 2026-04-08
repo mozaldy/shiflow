581ac1a6653af503d636177b0c38960073847046
nic <nicoleclarence150404@gmail.com>
2026-04-08 17:04:36 +0700

    final button of primary, dissmiss, next, and back

---
+++ shiflow/Components/Buttons/BackButton.swift
@@ -0,0 +1,18 @@
+//
+//  BackButton.swift
+//  shiflow
+//
+//  Created by Nicole Clarence on 08/04/26.
+//
+
+import SwiftUI
+
+struct BackButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BackButton()
+}
