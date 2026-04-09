5d06127c998e87092201910ee3aed425cba43f9a
mozaldy <moxaldy@gmail.com>
2026-04-09 14:17:34 +0700

    initialize: add finger practice screens

---
+++ shiflow/Screens/Practice/FingerRapidFireScreen.swift
@@ -0,0 +1,18 @@
+//
+//  FingerRapidFireScreen.swift
+//  shiflow
+//
+//  Created by Mohammad Rizaldy Ramadhan on 09/04/26.
+//
+
+import SwiftUI
+
+struct FingerRapidFireScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FingerRapidFireScreen()
+}
